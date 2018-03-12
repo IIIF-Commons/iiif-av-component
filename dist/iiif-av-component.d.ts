36edc069dd3eecc500b45fbec66f01df457bf733
edsilv <e.silverton@gmail.com>
2018-03-12 13:43:35 +0000

    build

--- dist/iiif-av-component.d.ts
+++ dist/iiif-av-component.d.ts
@@ -1,4 +1,4 @@
-// iiif-av-component v0.0.25 https://github.com/iiif-commons/iiif-av-component#readme
+// iiif-av-component v0.0.26 https://github.com/iiif-commons/iiif-av-component#readme
 
 /// <reference types="base-component" />
 declare namespace IIIFComponents {
