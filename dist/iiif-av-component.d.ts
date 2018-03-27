00b24e0e8174edcbd1a02705bf03806fc0e40d68
edsilv <e.silverton@gmail.com>
2018-03-27 16:26:33 +0100

    build

--- dist/iiif-av-component.d.ts
+++ dist/iiif-av-component.d.ts
@@ -1,4 +1,4 @@
-// iiif-av-component v0.0.33 https://github.com/iiif-commons/iiif-av-component#readme
+// iiif-av-component v0.0.34 https://github.com/iiif-commons/iiif-av-component#readme
 interface Array<T> {
     /**
      * Determines whether an array includes a certain element, returning true or false as appropriate.
