f3640f5fafd202c9d74c9b063b0b9355a2179bdd
edsilv <e.silverton@gmail.com>
2018-04-23 10:40:50 +0100

    added mediaSyncMarginSecs

--- src/CanvasInstance.ts
+++ src/CanvasInstance.ts
@@ -35,6 +35,7 @@ namespace IIIFComponents {
         private _isStalled: boolean = false;
         private _lowPriorityFrequency: number = 250;
         private _lowPriorityInterval: number;
+        private _mediaSyncMarginSecs: number = 5;
         private _ranges: AVComponentObjects.CanvasRange[] = [];
         private _readyCanvasesCount: number = 0;
         private _stallRequestedBy: any[] = []; //todo: type
@@ -1048,7 +1049,7 @@ namespace IIIFComponents {
                     const factualTime: number = contentAnnotation.element[0].currentTime;
 
                     // off by 0.2 seconds
-                    if (Math.abs(factualTime - correctTime) > 0.4) {
+                    if (Math.abs(factualTime - correctTime) > this._mediaSyncMarginSecs) {
 
                         contentAnnotation.outOfSync = true;
                         //this.playbackStalled(true, contentAnnotation);
