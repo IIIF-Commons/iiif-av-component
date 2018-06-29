b52aaa7486df7fb33429eeb06240fb9f678b99e1
edsilv <e.silverton@gmail.com>
2018-06-29 15:47:06 +0100

    build

--- examples/js/iiif-av-component.js
+++ examples/js/iiif-av-component.js
@@ -323,16 +323,17 @@ var IIIFComponents;
         AVComponent.prototype.showCanvas = function (canvasId) {
             // if the passed canvas id is already the current canvas id, but the canvas isn't visible
             // (switching from virtual canvas)
-            //const currentCanvas: CanvasInstance | undefined = this._getCurrentCanvas();
-            // if (currentCanvas && currentCanvas.getCanvasId() === canvasId && !currentCanvas.isVisible()) {
-            //     currentCanvas.set({
-            //         visible: true
-            //     });
-            // } else {
-            this.set({
-                canvasId: canvasId
-            });
-            // }
+            var currentCanvas = this._getCurrentCanvas();
+            if (currentCanvas && currentCanvas.getCanvasId() === canvasId && !currentCanvas.isVisible()) {
+                currentCanvas.set({
+                    visible: true
+                });
+            }
+            else {
+                this.set({
+                    canvasId: canvasId
+                });
+            }
         };
         AVComponent.prototype._logMessage = function (message) {
             this.fire(AVComponent.Events.LOG, message);
