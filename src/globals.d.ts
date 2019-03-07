fe3081565de81f8055a8628c6a976a3adc31766f
edsilv <e.silverton@gmail.com>
2019-03-07 16:28:27 +0000

    updated globals

---
+++ src/globals.d.ts
@@ -0,0 +1,12 @@
+interface Window {
+    MediaSource: any;
+    WebKitMediaSource: any;
+    jQuery: any;
+}
+
+declare global {
+    const dashjs: any;
+    const Hls: any;
+    const WaveformData: any;
+}
+export {};
