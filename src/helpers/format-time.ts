f6567fc60cf179777dc9c4b22f6dc310c32ddb3a
Stephen Fraser <stephen.fraser@digirati.com>
2023-03-22 14:08:22 +0000

    Fixed format time

--- src/helpers/format-time.ts
+++ src/helpers/format-time.ts
@@ -1,4 +1,18 @@
-export function formatTime(aNumber: number): string {
+export function formatTime(input: number): string {
+  let aNumber = Math.max(0, input);
+
+  if (!Number.isSafeInteger(aNumber)) {
+    if (aNumber < 0.5) {
+      aNumber = 0;
+    } else {
+      aNumber = Number(Math.round(aNumber * 100) / 100);
+      // Covers Infinity and NaN
+      if (!Number.isSafeInteger(aNumber)) {
+        aNumber = 0;
+      }
+    }
+  }
+
   let hours: number | string, minutes: number | string, seconds: number | string, hourValue: string;
 
   seconds = Math.ceil(aNumber);
