4c5104726f2f5cdaef28483757731d14ab18f67b
Eliot Jordan <eliotj@princeton.edu>
2023-08-18 12:50:35 -0500

    Fix hls format detection
    Co-authored-by: Anna Headley <hackartisan@users.noreply.github.com>

--- src/helpers/can-play-hls.ts
+++ src/helpers/can-play-hls.ts
@@ -1,6 +1,18 @@
-import { getHls } from './get-hls';
+import { hlsMimeTypes } from './hls-media-types'
+import { getHls } from './get-hls'
 
 export function canPlayHls() {
-  const Hls = getHls();
-  return Hls && Hls.isSupported();
+  const Hls = getHls()
+  const doc = typeof document === 'object' && document
+  const videoElement = doc && doc.createElement('video')
+
+  return (
+    hlsMimeTypes.some((canItPlay: string) => {
+      if (videoElement) {
+        return /maybe|probably/i.test(videoElement.canPlayType(canItPlay))
+      } else {
+        return false
+      }
+    }) || Hls && Hls.isSupported()
+  )
 }
